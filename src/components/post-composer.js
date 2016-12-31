9f37e2b49912ed87f1f3980fca188495259270f5
Minku Lee <premist@me.com>
2016-12-31 21:09:59 +0900

    Add PostComposer

---
+++ src/components/post-composer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './post-composer.less';
+
+class PostComposer extends Component {
+  render() {
+    return(
+      <section className="post-composer">
+        <div className="container">
+          <p>PostComposer</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default PostComposer;
