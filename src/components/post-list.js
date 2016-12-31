b0d5a5bba37fb3f83e335abf0f10be2c4200fd6f
Minku Lee <premist@me.com>
2017-01-01 00:50:31 +0900

    WIP

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import fire from '../fire';
+
 import './post-list.less';
 
 class PostList extends Component {
+  state = { posts: [] };
+  postsRef = null;
+
+  constructor(props) {
+    super(props);
+
+    this.postsRef = fire.database()
+                        .ref('/posts');
+  }
+
+  componentDidMount() {
+    this.postsRef
+      .orderByChild('createdAt')
+      .limitToLast(25)
+      .once('value')
+      .then((snapshot) => this.setState({ posts: snapshot.val() }));
+  }
+
   render() {
+    const posts = Object.keys(this.state.posts).reverse().map((k) => {
+      return <li key={k}>{this.state.posts[k].content}</li>
+    });
+
     return (
       <section className="post-list">
         <div className="container">
           <p>PostList</p>
+          <ul>
+            {posts}
+          </ul>
         </div>
       </section>
     );
